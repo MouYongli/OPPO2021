67e1c6b1a8d52afaace11ca609062f8807febf6d
Yongli Mou <yongli.mou@rwth-aachen.de>
2021-09-14 02:14:00 +0200

    Update datasets and trainer

--- advnet/datasets.py
+++ advnet/datasets.py
@@ -5,15 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import skimage.io
 import skimage.transform
-import cv2
 import random
+import torch
 from torch.utils.data import Dataset
 
 class TripletDataset(Dataset):
     """
     Crack500 dataset
     """
-    def __init__(self, args, base_dir=None, split='train'):
+    def __init__(self, args, base_dir=None, split='train', transform=True):
         """
         :param base_dir: path to crack500 dataset directory: crop_size, train_crop_strategy, val_crop_strategy, test_crop_strategy
         :param split: train/val/test
@@ -27,6 +27,7 @@ class TripletDataset(Dataset):
             self.base_dir = osp.join(pwd, '../data/images')
         self.split = split
         self.args = args
+        self.transform = transform
 
         image_files = []
         for subdir in os.listdir(self.base_dir):
@@ -54,16 +55,84 @@ class TripletDataset(Dataset):
             img = skimage.transform.resize(img, (self.args.img_size, self.args.img_size))
             same_img = skimage.transform.resize(same_img, (self.args.img_size, self.args.img_size))
             other_img = skimage.transform.resize(other_img, (self.args.img_size, self.args.img_size))
-            sample = {"person_id": person_id, "example_id": example_id, "img": img, "same_img": same_img, "other_img": other_img}
+            if self.transform:
+                img, same_img, other_img = self.numpy_to_tensor(img), self.numpy_to_tensor(same_img), self.numpy_to_tensor(other_img)
+            sample = {"person_id": int(person_id), "example_id": int(example_id), "img": img, "same_img": same_img, "other_img": other_img}
         elif self.split == "test":
             person_id, example_id = self.images_df.loc[idx, "person_id"], self.images_df.loc[idx, "example_id"]
             img = skimage.io.imread(osp.join(self.base_dir, "{}/{}_{}.jpg".format(person_id, person_id, example_id)))
-            sample = {"person_id": person_id, "example_id": example_id, "img": img}
+            img = skimage.transform.resize(img, (self.args.img_size, self.args.img_size))
+            if self.transform:
+                img = self.numpy_to_tensor(img)
+            sample = {"person_id": int(person_id), "example_id": int(example_id), "img": img}
         else:
             raise  NotImplementedError
         return sample
 
+    def numpy_to_tensor(self, img):
+        img = img.astype(np.float64)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).float()
+        return img
 
+    def tensor_to_numpy(self, img):
+        img = img.numpy()
+        img = img.transpose(1, 2, 0)
+        img = img.astype(np.uint8)
+        # img = img[:, :, ::-1]
+        return img
+
+class FaceDataset(Dataset):
+    """
+    Crack500 dataset
+    """
+    def __init__(self, args, base_dir=None, split='train', transform=True):
+        """
+        :param base_dir: path to crack500 dataset directory: crop_size, train_crop_strategy, val_crop_strategy, test_crop_strategy
+        :param split: train/val/test
+        :param transform: transform to apply
+        """
+        super().__init__()
+        if base_dir is not None:
+            self.base_dir = base_dir
+        else:
+            pwd = osp.dirname(osp.abspath(__file__))
+            self.base_dir = osp.join(pwd, '../data/images')
+        self.split = split
+        self.args = args
+        self.transform = transform
+        image_files = []
+        for subdir in os.listdir(self.base_dir):
+            for file_name in os.listdir(osp.join(self.base_dir, subdir)):
+                if file_name.endswith('.jpg'):
+                    image_files.append(["{}/{}".format(subdir, file_name), subdir, file_name[-5:-4]])
+        self.images_df = pd.DataFrame(image_files, columns=["file_path", "person_id", "example_id"])
+
+    def __len__(self):
+        return len(self.images_df)
+
+    def __getitem__(self, idx):
+        person_id, example_id = self.images_df.loc[idx, "person_id"], self.images_df.loc[idx, "example_id"]
+        img = skimage.io.imread(osp.join(self.base_dir, "{}/{}_{}.jpg".format(person_id, person_id, example_id)))
+        height, width = img.shape[0], img.shape[1]
+        img = skimage.transform.resize(img, (self.args.img_size, self.args.img_size))
+        if self.transform:
+            img = self.numpy_to_tensor(img)
+        sample = {"person_id": int(person_id), "example_id": int(example_id), "height": height, "width":width, "img": img}
+        return sample
+
+    def numpy_to_tensor(self, img):
+        img = img.astype(np.float64)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).float()
+        return img
+
+    def tensor_to_numpy(self, img):
+        img = img.numpy()
+        img = img.transpose(1, 2, 0)
+        img = img.astype(np.uint8)
+        # img = img[:, :, ::-1]
+        return img
 
 if __name__ == '__main__':
     import argparse
@@ -72,14 +141,21 @@ if __name__ == '__main__':
                         help='model name (default: deeplab)')
     parser.add_argument('--img-size', type=int, default=112, help='image size (default: 112)')
     args = parser.parse_args()
+    train_triplet_dataset = TripletDataset(args, split="train")
+    sample = train_triplet_dataset[0]
+    # img, same_img, other_img = sample["img"], sample["same_img"], sample["other_img"]
+    # plt.subplot(131)
+    # plt.imshow(img)
+    # plt.subplot(132)
+    # plt.imshow(same_img)
+    # plt.subplot(133)
+    # plt.imshow(other_img)
+    # plt.show()
 
-    dataset = TripletDataset(args)
-    sample = dataset[0]
-    img, same_img, other_img = sample["img"], sample["same_img"], sample["other_img"]
-    plt.subplot(131)
+    test_triplet_dataset = TripletDataset(args, split="test")
+    sample = test_triplet_dataset[0]
+    img, person_id, example_id = sample["img"], sample["person_id"], sample["example_id"]
+    print("person id", person_id, example_id)
+    print(person_id.__repr__)
     plt.imshow(img)
-    plt.subplot(132)
-    plt.imshow(same_img)
-    plt.subplot(133)
-    plt.imshow(other_img)
     plt.show()
--- advnet/trainer.py
+++ advnet/trainer.py
@@ -2,13 +2,12 @@ import argparse
 import os
 import os.path as osp
 import numpy as np
-import datetime
 import glob
 import torch
 from torch.autograd import Variable
-from datasets import TripletDataset
-from models import EmbeddingNet
-from losses import TripletLoss, ContrastiveLoss
+from datasets import TripletDataset, FaceDataset
+from models import EmbeddingNet, GaussianBlur, NoiseGenerator
+from losses import TripletLoss
 
 here = osp.dirname(osp.abspath(__file__))
 
@@ -32,17 +31,49 @@ def train_triplet_epoch(epoch, train_loader, model, loss_fn, optimizer, cuda, ex
         log = map(str, log)
         f.write(','.join(log) + '\n')
 
-def test_triplet_epoch(epoch, test_loader, model, loss_fn, cuda, experiment_dir):
-    model.eval()
-    test_loss = 0.0
-    for batch_idx, sample in enumerate(test_loader):
-        anchor, positive, negative = sample["img"], sample["same_img"], sample["other_img"]
+# def test_triplet_epoch(epoch, test_loader, model, cuda, experiment_dir):
+#     model.eval()
+#     test_loss = 0.0
+#     test_embedding_vectors = []
+#     test_pids = []
+#     for batch_idx, sample in enumerate(test_loader):
+#         imgs, person_id, example_id = sample["img"], sample["person_id"], sample["example_id"]
+#         if cuda:
+#             imgs = imgs.cuda()
+#         imgs = Variable(imgs)
+#         embedding_imgs = model(imgs)
+#         for img, pid in zip(embedding_imgs, person_id):
+#             test_embedding_vectors.append(img)
+#             test_pids.append(img)
+#     for pid in np.unique(test_pids):
+#         pass
+
+def train_adv_epoch(epoch, train_loader, advnet, gaussian_blur, embedding_net, adv_optim, cuda, experiment_dir, alpha_contra_mse, alpha_):
+    advnet.train()
+    gaussian_blur.train()
+    embedding_net.train()
+    train_loss = 0.0
+    for batch_idx, sample in enumerate(train_loader):
+        imgs, person_id, example_id = sample["img"], sample["person_id"], sample["example_id"]
         if cuda:
-            anchor, positive, negative = anchor.cuda(), positive.cuda(), negative.cuda()
-        anchor, positive, negative = Variable(anchor), Variable(positive), Variable(negative)
-        embedding_anchor, embedding_positive, embedding_negative = model(anchor), model(positive), model(negative)
-        loss = loss_fn(embedding_anchor, embedding_positive, embedding_negative)
+            imgs = imgs.cuda()
+        imgs = Variable(imgs)
+        adv_optim.zero_grad()
+        noise = gaussian_blur(advnet(imgs))
+        noised_img = imgs + noise
+        contra_mse = (embedding_net(imgs) - embedding_net(noised_img)).pow(2).sum(1)
+        diff1 = noised_img[:, :, :, :-1] - noised_img[:, :, :, 1:]
+        diff2 = noised_img[:, :, :-1, :] - noised_img[:, :, 1:, :]
+        diff3 = noised_img[:, :, 1:, :-1] - noised_img[:, :, :-1, 1:]
+        diff4 = noised_img[:, :, :-1, :-1] - noised_img[:, :, 1:, 1:]
+        tv_l2 = torch.norm(diff1) + torch.norm(diff2) + torch.norm(diff3) + torch.norm(diff4)
+        tv_l1 = diff1.abs().mean() + diff2.abs().mean() + diff3.abs().mean() + diff4.abs().mean()
+        noise_l2 = torch.norm(noise)
+        embedded_l2 = torch.norm(embedding_net(noise))
+
         train_loss += loss.data.item()
+        loss.backward()
+        adv_optim.step()
     train_loss /= len(train_loader)
     with open(osp.join(experiment_dir.out, 'triplet_log.csv'), 'a') as f:
         log = [epoch, train_loss]
@@ -59,6 +90,7 @@ if __name__ == '__main__':
     parser.add_argument('--weight-decay', type=float, default=0.0005, help='weight decay',)
     parser.add_argument('--seed', type=int, default=1337, help='seed for randomness',)
     parser.add_argument('--margin', type=float, default=1.0, help='margin in loss function',)
+    parser.add_argument('--kernel-size', type=float, default=1.0, help='margin in loss function',)
     args = parser.parse_args()
 
     # Set up experiment dir
@@ -85,22 +117,31 @@ if __name__ == '__main__':
 
     # Define dataset
     kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
-    train_triplet_loader = torch.utils.data.DataLoader(TripletDataset(args, split='train'), batch_size=8, shuffle=True, **kwargs)
-    valid_triplet_loader = torch.utils.data.DataLoader(TripletDataset(args, split='valid'), batch_size=1, shuffle=False, **kwargs)
-
+    train_triplet_loader = torch.utils.data.DataLoader(TripletDataset(args, split='train', transform=True), batch_size=8, shuffle=True, **kwargs)
+    test_triplet_loader = torch.utils.data.DataLoader(TripletDataset(args, split='test', transform=True), batch_size=8, shuffle=False, **kwargs)
+    train_adv_loader = torch.utils.data.DataLoader(FaceDataset(args, split='train', transform=True), batch_size=8, shuffle=True, **kwargs)
+    test_adv_loader = torch.utils.data.DataLoader(FaceDataset(args, split='test', transform=True), batch_size=8, shuffle=False, **kwargs)
     # Define model
-    dnet = EmbeddingNet()
+    embedding_net = EmbeddingNet()
+    advnet = NoiseGenerator()
+    gaussian_blur = GaussianBlur(args.kernel_size)
     if cuda:
-        dnet = dnet.cuda()
+        embedding_net = embedding_net.cuda()
+        advnet = advnet.cuda()
+        gaussian_blur = gaussian_blur.cuda()
+
 
     # Define optimizer
-    triplet_optim = torch.optim.Adam(dnet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    triplet_optim = torch.optim.Adam(embedding_net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    adv_optim = torch.optim.Adam(advnet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
     # Define loss function
     triplet_loss = TripletLoss(args.margin)
-    contrastive_loss = ContrastiveLoss(args.margin)
 
     for epoch in range(args.pretrain_epochs):
-        train_triplet_epoch(train_triplet_loader, dnet, triplet_loss, triplet_optim, cuda, experiment_dir)
+        train_triplet_epoch(epoch, train_triplet_loader, embedding_net, triplet_loss, triplet_optim, cuda, experiment_dir)
+
+    for epoch in range(args.epochs):
+        train_adv_epoch(epoch, train_adv_loader, advnet, gaussian_blur, embedding_net, adv_optim, cuda, experiment_dir)
 
 
